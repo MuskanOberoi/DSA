44e5ca99c6f43a4c41df8d02b9c6f574b8307582
MuskanOberoi <muskanoberoi27@gmail.com>
2025-04-18 23:07:21 +0530

    problem 1

---
+++ Patterns/Problem_1.java
@@ -0,0 +1,22 @@
+// print this pattern
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+package Patterns;
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in); 
+       System.out.println("Enter the num");
+       int n = sc.nextInt();
+       //outer loop for rows
+       for(int i=0; i<n; i++){
+        // inner loop for columns
+        for(int j=0; j<n; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+       }
+    }
+}
