20e43afdffe211db00e4d4dcc0cb1f7f789cdafb
MuskanOberoi <muskanoberoi27@gmail.com>
2025-05-05 23:44:35 +0530

    practice

---
+++ Basics/reverseNum.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class reverseNum {
+    {
+        
+         int n = 5678;
+       int revNum = 0;
+       while(n>0){
+        int lastdigit = n%10;
+        revNum = (revNum*10) + lastdigit;
+        n = n/10;
+       }
+       System.out.println("Reverse Number is: "+revNum);
+       }
+}
